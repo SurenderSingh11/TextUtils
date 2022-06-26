a0c99c45898d60e0ff34ec553bd7c9558269d2fa
SurenderSingh11 <srockaswal@gmail.com>
2022-06-26 21:48:36 +0530

    Enhanced dark mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,10 +35,10 @@ export default function TextForm(props) {
   // setText("new Text");
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
           <h1>{props.heading}</h1>
           <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" />
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8" />
           </div>
           <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
           <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
@@ -46,12 +46,12 @@ export default function TextForm(props) {
           <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy text</button>
           <button className="btn btn-primary mx-2" onClick={handleExtarSpaces}>Remove Extra Spaces</button>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>Your text summary </h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
       </div>
     </>
   )
